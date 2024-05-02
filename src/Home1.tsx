10efea582406c778d53c27f63dc3667c71750d09
Youssef Maged <youssef.maged.6000@gmail.com>
2024-05-02 17:29:19 +0300

    Revert "copying repo"
    This reverts commit 94a34686dc94b6a1c9eaa3453de67c8599c64939.

--- src/Home1.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home1 = () => {
-  return (
-    <div>Home1</div>
-  )
-}
-
-export default Home1
--- src/SignUpValid.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { z } from "zod";
-
-export const formSchema = z.object({
-    email: z.string().min(2,{message:'Too short'}),
-    password: z.string().min(2,{message:'Too short'}),
-})
